27ef756cdc56db2481149e67049e6f35135e22b3
CatherineBose <catherine.navin@gmail.com>
2017-12-22 07:22:49 +0000

    like updated

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,16 +8,21 @@ class LikesController < ApplicationController
   def create
     puts "In likes_controller create method :: for liking a post"
     @idea = Idea.find(params[:id])
-    @like= Like.new(user_id: current_user.id, idea_id: params[:id])
-    if @like.save
-      puts "like got saved"
-    redirect_to ideas_path
-    else
-      puts "like errored out"
-      flash[:errors] = @user.errors.full_messages
-      # redirect_to new_sessions_path
+    # if @idea.liked_users.include(current_user.id)
+    #   flash[:errors]<< "You have already liked the post"
+    #   redirect_to ideas_path
+    # end
+    # else
+      @like= Like.new(user_id: current_user.id, idea_id: params[:id])
+      if @like.save
+        puts "like got saved"
       redirect_to ideas_path
-    end
+      else
+        puts "like errored out"
+        flash[:errors] = @user.errors.full_messages
+        # redirect_to new_sessions_path
+        redirect_to ideas_path
+      end
   end
 
   def show
